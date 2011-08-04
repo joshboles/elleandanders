3fbef4ba7c4d4655424b1aca87e6254857436b64
Josh Boles <github@joshboles.com>
2011-08-04 10:44:17 -0400

    Wrong settings. Fixed.

--- elleandanders_site/deploy/wsgi.py
+++ elleandanders_site/deploy/wsgi.py
@@ -1,5 +1,5 @@
 import os
 
 from django.core.handlers.wsgi import WSGIHandler
-os.environ["DJANGO_SETTINGS_MODULE"] = "elleandanders.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "elleandanders_site.settings"
 application = WSGIHandler()
