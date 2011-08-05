7f94c95b6fe8a15358f30e804bb78e287574e1b2
Josh Boles <github@joshboles.com>
2011-08-05 16:56:41 -0400

    Fixed domain.

--- elleandanders_site/middleware.py
+++ elleandanders_site/middleware.py
@@ -2,8 +2,8 @@ from django.http import HttpResponseRedirect
 
 class NoWWW:
     def process_request(self, request):
-        if request.get_host() != "elleandanders.com":
-            return HttpResponseRedirect("http://elleandanders.com%s" % request.get_full_path())
+        if request.get_host() != "elleandandy.com":
+            return HttpResponseRedirect("http://elleandandy.com%s" % request.get_full_path())
     
         if request.get_full_path() == "/favicon.ico":
             return HttpResponseRedirect("/static/img/favicon.ico")
