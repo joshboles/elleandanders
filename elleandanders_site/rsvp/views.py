b2a2df537085c31bc6eae1ce1a6af8d42c3b7310
Josh Boles <github@joshboles.com>
2011-08-15 22:15:11 -0400

    Tweaks to styles, javascript, and messages.

--- elleandanders_site/rsvp/views.py
+++ elleandanders_site/rsvp/views.py
@@ -7,17 +7,17 @@ from rsvp.forms import RsvpForm
 
 
 def rsvp(request):
-    
+
     # Handles the DinnerChoice
     DinnerChoiceFormset = inlineformset_factory(Rsvp, DinnerChoice, extra=4, can_delete=False)
     formset = DinnerChoiceFormset()
 
     # Handles the RSVP
     form = RsvpForm(request.POST or None)
-    
+
     if form.is_valid():
         rsvp = form.save()
-        
+
         if rsvp.dinner_dancing:
             # recreate now that we have an instance
             formset = DinnerChoiceFormset(request.POST, instance=rsvp)
@@ -25,10 +25,10 @@ def rsvp(request):
                 formset.save()
             else:
                 print formset.errors
-        
+
         messages.success(request, "Your RSVP was received.")
         return redirect("/")
-    
+
     return render_to_response("rsvp/form.html", {
         "form": form,
         "formset": formset,
